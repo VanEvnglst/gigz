d7b42b5650555c64b7b7516bb1cd8fd737a69946
Howsy - Ivan <howsy-ivan@Howsys-MacBook-Pro.local>
2020-07-13 22:07:40 +0800

    Add Images to theme

--- App/Theme/Images.js
+++ App/Theme/Images.js
@@ -3,6 +3,12 @@
  */
 
 export default {
-  logo: require('App/Assets/Images/TOM.png'),
+  logo: require('App/Assets/Images/gigz-logo.png'),
+  logoDark: require('App/Assets/Images/gigz-logo-dark.png'),
   cardImage: require('App/Assets/Images/cardImage.png'),
+  splash: require('App/Assets/Images/splash-bg.png'),
+  onboardingStep1: require('App/Assets/Images/onboarding-1.png'),
+  onboardingStep2: require('App/Assets/Images/onboarding-2.png'),
+  onboardingStep3: require('App/Assets/Images/onboarding-3.png'),
+  loginBanner: require('App/Assets/Images/login-banner.png'),
 }
