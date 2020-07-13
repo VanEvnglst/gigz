3ff41dc835f1aa6d67ec389eda3c641cec4390e0
Howsy - Ivan <howsy-ivan@Howsys-MacBook-Pro.local>
2020-07-13 22:07:52 +0800

    rework splash screen

--- App/Containers/SplashScreen/index.js
+++ App/Containers/SplashScreen/index.js
@@ -1,19 +1,25 @@
 import React, { useEffect } from 'react'
-import { Text, View } from 'react-native'
+import { Text, View, Image, ImageBackground } from 'react-native'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
+import { Images } from '../../Theme';
 
 import styles from './styles'
 
 const SplashScreen = ({ navigation, login }) => {
   useEffect(() => {
-    const redirectToPage = login.data ? 'HomeScreen' : 'LoginScreen'
+    const redirectToPage = login.data ? 'HomeScreen' : 'OnboardingScreen'
     setTimeout(() => navigation.navigate(redirectToPage), 1000)
   }, [])
 
   return (
     <View style={styles.container}>
-      <Text style={styles.logoStyle}>Zing</Text>
+      <ImageBackground style={styles.imageContainer} source={Images.splash}>
+        <View style={styles.contentContainer}>
+      <Image style={styles.logoStyle} source={Images.logoDark}/>
+      <Text style={styles.textStyle}>The next stage</Text>
+      </View>
+      </ImageBackground>
     </View>
   )
 }
--- App/Containers/SplashScreen/styles.js
+++ App/Containers/SplashScreen/styles.js
@@ -2,11 +2,30 @@ import { StyleSheet } from 'react-native'
 
 export default StyleSheet.create({
   container: {
+    flex: 1
+  },
+  imageContainer: {
+    flex: 1, 
+    alignSelf: 'stretch',
+    backgroundColor: '#2C2C2C',
+  },
+  overlay: {
+    backgroundColor: '#2C2C2C',
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    opacity: 0.7
+  },
+  contentContainer: {
+    flex: 1, 
+    marginTop: 100, 
+    alignItems: 'center'
+  },
+  textStyle: {
+    fontSize: 14, 
+    color: '#FFFFFF', 
+    opacity: 0.7, 
+    marginTop: 25 
   },
   logoStyle: {
-    fontSize: 25,
+    backgroundColor: 'transparent'
   },
 })
