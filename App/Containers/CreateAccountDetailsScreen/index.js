6975d1ef2561ac103fbeaaed70f0c23f311e4f3e
Howsy - Ivan <howsy-ivan@Howsys-MacBook-Pro.local>
2020-07-18 22:51:38 +0800

    Apply keyboard avoiding view

--- App/Containers/CreateAccountDetailsScreen/index.js
+++ App/Containers/CreateAccountDetailsScreen/index.js
@@ -1,46 +1,61 @@
 import React from 'react';
-import { View, Text, TextInput, TouchableOpacity } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity, Platform, KeyboardAvoidingView } from 'react-native';
+import CountryPicker from 'react-native-country-picker-modal'
 import styles from './styles';
 
 const CreateAccountDetailsScreen = props => {
   const { navigation } = props;
   return (
-    <View style={{ flex: 1 }}>
-      <View style={{flex: 1, marginHorizontal: 30, marginTop: 60 }}>
-        <View style={{alignItems: 'flex-start'}}>
-        <Text style={{ paddingLeft: 10, fontWeight: 'bold', color: "#1D262C", lineHeight: 20, fontSize: 16}}>Create Account</Text>
-
-        <TextInput
-        style={styles.inputStyle}
-        placeholder={'Enter your mobile number'}/>
-         <TextInput
-        style={styles.inputStyle}
-        placeholder={'Full name'}/>
-         <TextInput
-        style={styles.inputStyle}
-        placeholder={'Email address'}/>
-        <Text style={{ paddingLeft: 10, marginTop: 20, fontWeight: 'bold', color: "#1D262C",lineHeight: 20, fontSize: 16 }}>Profile Photo</Text>
-
-        <TouchableOpacity
-        style={{borderStyle: 'dashed', borderColor: '#70707029',
-        borderWidth: 1,
-        borderRadius: 25, width: '90%', alignItems: 'center', height: 51, justifyContent: 'center', marginTop: 10}}>
-          <Text>Take a Photo, or Browse</Text>
-         
-        </TouchableOpacity>
-        <Text style={{ alignSelf: 'center'}}>Maximum upload file size 10MB.</Text>
-        <View style={{ marginTop: 60, width: '100%',alignItems: 'center', }}>
-        <TouchableOpacity
-          style={styles.buttonStyle}
-          onPress={() => navigation.navigate('HomeScreen')}>
-          <Text style={styles.buttonTextStyle}>Create Account</Text>
-        </TouchableOpacity>
+    <KeyboardAvoidingView
+      behavior={Platform.OS == "ios" ? "padding" : "height"}
+      keyboardVerticalOffset={Platform.OS == "ios" ? 0 : 20}
+      enabled={Platform.OS === "ios" ? true : false}
+      style={styles.container}>
+      <View style={styles.contentContainer}>
+        <View style={{ alignItems: 'flex-start' }}>
+          <Text style={styles.headerTextStyle}>Create Account</Text>
           
-        </View>
+        
+          <View style={{ borderWidth: 1, width: '100%',
+    borderColor: '#70707029',
+    marginVertical: 10, height: 51, borderRadius: 25, flexDirection: 'row', justifyContent: 'flex-start', alignItems: 'center',}}>
+          <CountryPicker
+            containerButtonStyle={{height: 51, borderTopLeftRadius: 25, borderBottomLeftRadius: 25, width: '80%', justifyContent: 'center', paddingLeft: 15, backgroundColor: '#70707029'}}
+            withFlag={true}
+          />
+          <TextInput
+            placeholder={'Mobile number'}
+            keyboardType={'phone-pad'}
+          />
+         
+            </View>
+          <TextInput
+            style={styles.inputStyle}
+            placeholder={'Full name'} />
+          <TextInput
+            style={styles.inputStyle}
+            keyboardType={'email-address'}
+            placeholder={'Email address'} />
+          <Text style={styles.profileTextStyle}>Profile Photo</Text>
+
+          <TouchableOpacity
+            style={styles.profileButtonStyle}>
+            <Text style={styles.profileBtnTextStyle}>Take a Photo, or</Text>
+            <Text style={styles.profileBtnRedTextStyle}> Browse</Text>
+
+          </TouchableOpacity>
+          <Text style={styles.noteTextStyle}>Maximum upload file size 10MB.</Text>
+          <View style={styles.buttonContainer}>
+            <TouchableOpacity
+              style={styles.buttonStyle}
+              onPress={() => navigation.navigate('HomeScreen')}>
+              <Text style={styles.buttonTextStyle}>Create Account</Text>
+            </TouchableOpacity>
+
+          </View>
         </View>
       </View>
-      
-    </View>
+    </KeyboardAvoidingView>
   )
 
 }
--- App/Containers/CreateAccountDetailsScreen/styles.js
+++ App/Containers/CreateAccountDetailsScreen/styles.js
@@ -1,14 +1,74 @@
 import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  contentContainer: {
+    flex: 1, 
+    marginHorizontal: 30,
+    marginTop: 60
+  },
+  headerTextStyle: { 
+    paddingLeft: 10, 
+    fontWeight: 'bold', 
+    color: "#1D262C", 
+    lineHeight: 20, 
+    fontSize: 16
+  },
   inputStyle: { 
     borderWidth: 1, 
-    width: '90%', 
+    width: '100%', 
     height: 51, 
     borderRadius: 25, 
     padding: 15, 
     borderColor: '#70707029',
     marginVertical: 10
+  },
+  profileTextStyle: { 
+    paddingLeft: 10, 
+    marginTop: 20, 
+    fontWeight: 'bold', 
+    color: "#1D262C",
+    lineHeight: 20, 
+    fontSize: 16 
+  },
+  profileButtonStyle: {
+    borderStyle: 'dashed', 
+    borderColor: '#70707029',
+    borderWidth: 1,
+    borderRadius: 25, 
+    width: '100%', 
+    alignItems: 'center', 
+    height: 51, 
+    justifyContent: 'center', 
+    marginTop: 10,
+    flexDirection: 'row'
+  },
+  profileBtnTextStyle: { 
+    color: '#333333', 
+    opacity: 0.3, 
+    lineHeight: 21, 
+    fontSize: 12, 
+  },
+  profileBtnRedTextStyle: { 
+    color: '#FA3830',  
+    lineHeight: 21, 
+    fontSize: 12, 
+    fontWeight: '600'
+  },
+  noteTextStyle: { 
+    alignSelf: 'center',
+    lineHeight: 21, 
+    fontSize: 10,
+    color: '#333333', 
+    opacity: 0.3,
+    marginTop: 5,
+  },
+  buttonContainer: { 
+    marginTop: 60, 
+    width: '100%',
+    alignItems: 'center', 
   },
   buttonStyle: { 
     height: 51, 
